18471bef03f62b3ad6c9e33cd24f404b188c298e
dafma <mar_tp@hotmail.es>
2016-09-22 18:33:34 -0500

    v5

--- CapaDatos/DCategoria.cs
+++ CapaDatos/DCategoria.cs
@@ -3,10 +3,123 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace CapaDatos
 {
     class DCategoria
     {
+        private int _Idcategoria;
+        private string _Nombre;
+        private string _Descripcion;
+        private string _TextoBuscar;
+
+        public int Idcategoria
+        {
+            get
+            {
+                return _Idcategoria;
+            }
+
+            set
+            {
+                _Idcategoria = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return _Nombre;
+            }
+
+            set
+            {
+                _Nombre = value;
+            }
+        }
+
+        public string Descripcion
+        {
+            get
+            {
+                return _Descripcion;
+            }
+
+            set
+            {
+                _Descripcion = value;
+            }
+        }
+
+        public string TextoBuscar
+        {
+            get
+            {
+                return _TextoBuscar;
+            }
+
+            set
+            {
+                _TextoBuscar = value;
+            }
+        }
+        //constructor vacio
+        public DCategoria()
+        {
+
+        }
+        //Constriuctor con parametros
+        public DCategoria(int idcategoria, string nombre, string descripcion, string textobuscar)
+        {
+            this.Idcategoria = idcategoria;
+            this.Nombre = nombre;
+            this.Descripcion = descripcion;
+            this.TextoBuscar = textobuscar;
+        }
+        // metodo insertar
+        public string Insertar(DCategoria Categoria)
+        {
+            string rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Codigo
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+
+
+            }catch(Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+        // metodo editar
+        public string Editar(DCategoria Categoria)
+        {
+
+        }
+        //Metodo Eliminar 
+        public string Eliminar(DCategoria Categoria)
+        {
+
+        }
+        //metodo mostrar
+        public DataTable Mostrar()
+        {
+
+        }
+        // metodod buscar nombre
+        public DataTable BuscarNombre(DCategoria Categoria)
+        {
+
+        }
     }
 }
